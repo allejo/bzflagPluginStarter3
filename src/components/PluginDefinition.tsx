81dce988afdb9a21b8164881c6943194d9fccd60
Vladimir Jimenez <allejo@me.com>
2022-08-12 15:30:35 -0700

    Port PluginDefinition into a functional component

--- src/components/PluginDefinition.tsx
+++ src/components/PluginDefinition.tsx
@@ -1,5 +1,5 @@
 import { ILicense } from '@allejo/bzf-plugin-gen';
-import React, { Component, SyntheticEvent } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 
 import Licenses from '../data/licenses.json';
 
@@ -16,106 +16,90 @@ interface Props {
   onUpdate(info: PluginDefinitionData): void;
 }
 
-interface State {
-  info: PluginDefinitionData;
-}
+type LicenseName = keyof typeof Licenses;
 
-type ILicenseMap = { [key: string]: ILicense };
+const PluginDefinition = ({ onUpdate }: Props) => {
+  const [pluginName, setPluginName] = useState('');
+  const [pluginAuthor, setPluginAuthor] = useState('');
+  const [pluginLicense, setPluginLicense] = useState(Licenses.Proprietary);
+  const [playerCallsign, setPlayerCallsign] = useState('');
+  const licenses = useMemo(
+    () =>
+      Object.values(Licenses).map((license: ILicense, index: number) => (
+        <option key={index} value={license.name}>
+          {license.name}
+        </option>
+      )),
+    [Licenses],
+  );
 
-export default class PluginDefinition extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {
-      info: {
-        pluginName: '',
-        pluginAuthor: '',
-        pluginLicense: Licenses.Proprietary,
-        playerCallsign: '',
-      },
-    };
-  }
-
-  handleChange = (event: SyntheticEvent<HTMLInputElement | HTMLSelectElement>) => {
-    const info = Object.assign({}, this.state.info);
-
-    if (event.currentTarget instanceof HTMLInputElement) {
-      info[event.currentTarget.name] = event.currentTarget.value;
-    } else if (event.currentTarget instanceof HTMLSelectElement) {
-      info.pluginLicense = (Licenses as ILicenseMap)[event.currentTarget.value];
-    }
-
-    this.setState({
-      info,
+  useEffect(() => {
+    onUpdate({
+      pluginName,
+      pluginAuthor,
+      pluginLicense,
+      playerCallsign,
     });
+  }, [pluginName, pluginAuthor, pluginLicense, playerCallsign]);
 
-    this.props.onUpdate(info);
-  };
-
-  render() {
-    const licenses = Object.values(Licenses).map((license: ILicense, index: number) => (
-      <option key={index} value={license.name}>
-        {license.name}
-      </option>
-    ));
-
-    return (
-      <section className="row">
-        <div className="col-md-6">
-          <div className="form-group">
-            <label htmlFor="plugin-name">Plug-in Name</label>
-            <input
-              type="text"
-              id="plugin-name"
-              name="pluginName"
-              className="form-control"
-              placeholder="Sample Plug-in"
-              value={this.state.info.pluginName}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="plugin-author">Plug-in Author</label>
-            <input
-              type="text"
-              id="plugin-author"
-              name="pluginAuthor"
-              className="form-control"
-              placeholder="Jane Doe"
-              value={this.state.info.pluginAuthor}
-              onChange={this.handleChange}
-            />
-          </div>
+  return (
+    <section className="row">
+      <div className="col-md-6">
+        <div className="form-group">
+          <label htmlFor="plugin-name">Plug-in Name</label>
+          <input
+            type="text"
+            id="plugin-name"
+            name="pluginName"
+            className="form-control"
+            placeholder="Sample Plug-in"
+            value={pluginName}
+            onChange={e => setPluginName(e.currentTarget.value)}
+          />
         </div>
 
-        <div className="col-md-6">
-          <div className="form-group">
-            <label htmlFor="plugin-license">Plug-in License</label>
-            <select
-              className="custom-select"
-              id="plugin-license"
-              value={this.state.info.pluginLicense.name}
-              onChange={this.handleChange}
-            >
-              {licenses}
-            </select>
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="player-callsign">Player Callsign</label>
-            <input
-              type="text"
-              id="player-callsign"
-              name="playerCallsign"
-              className="form-control"
-              placeholder="allejo"
-              value={this.state.info.playerCallsign}
-              onChange={this.handleChange}
-            />
-          </div>
+        <div className="form-group">
+          <label htmlFor="plugin-author">Plug-in Author</label>
+          <input
+            type="text"
+            id="plugin-author"
+            name="pluginAuthor"
+            className="form-control"
+            placeholder="Jane Doe"
+            value={pluginAuthor}
+            onChange={e => setPluginAuthor(e.currentTarget.value)}
+          />
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+
+      <div className="col-md-6">
+        <div className="form-group">
+          <label htmlFor="plugin-license">Plug-in License</label>
+          <select
+            className="custom-select"
+            id="plugin-license"
+            value={pluginLicense.name}
+            onChange={e => setPluginLicense(Licenses[e.currentTarget.value as LicenseName])}
+          >
+            {licenses}
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="player-callsign">Player Callsign</label>
+          <input
+            type="text"
+            id="player-callsign"
+            name="playerCallsign"
+            className="form-control"
+            placeholder="allejo"
+            value={playerCallsign}
+            onChange={e => setPlayerCallsign(e.currentTarget.value)}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PluginDefinition;
