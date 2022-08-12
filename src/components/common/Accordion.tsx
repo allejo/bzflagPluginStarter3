a6c94beb872374373015866f4f9a472790870ff3
Vladimir Jimenez <allejo@me.com>
2022-08-12 14:55:58 -0700

    Convert Accordion into a functional component

--- src/components/common/Accordion.tsx
+++ src/components/common/Accordion.tsx
@@ -1,39 +1,36 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { Component, ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import { Collapse } from 'reactstrap';
 
 import styles from './Accordion.module.scss';
 
 interface Props {
-  isOpen: boolean;
-  heading: string;
   children: ReactNode;
-
+  heading: string;
+  isOpen: boolean;
   onToggle(isOpen: boolean): void;
 }
 
-export default class Accordion extends Component<Props> {
-  handleClick = () => {
-    this.props.onToggle(!this.props.isOpen);
-  };
-
-  render() {
-    return (
-      <article>
-        <header className={styles.header} aria-expanded={this.props.isOpen} onClick={this.handleClick}>
-          <h2 tabIndex={0}>
-            <span className={styles.icon} aria-hidden={true}>
-              <FontAwesomeIcon icon={this.props.isOpen ? 'angle-down' : 'angle-right'} />
-            </span>
-
-            {this.props.heading}
-          </h2>
-        </header>
-
-        <Collapse isOpen={this.props.isOpen}>
-          <div className={styles.body}>{this.props.children}</div>
-        </Collapse>
-      </article>
-    );
+const Accordion = ({ heading, isOpen, children, onToggle }: Props) => {
+  const handleClick = () => {
+    onToggle(!isOpen);
   }
-}
+
+  return (
+    <section>
+      <button className={styles.header} aria-expanded={isOpen} onClick={handleClick}>
+        <span className={styles.icon} aria-hidden={true}>
+          <FontAwesomeIcon icon={isOpen ? 'angle-down' : 'angle-right'} />
+        </span>
+
+        {heading}
+      </button>
+
+      <Collapse isOpen={isOpen}>
+        <div className={styles.body}>{children}</div>
+      </Collapse>
+    </section>
+  );
+};
+
+export default Accordion;
