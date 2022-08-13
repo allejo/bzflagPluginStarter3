65b7b8389fdedd8f4d051d88e944f30d50d121ec
Vladimir Jimenez <allejo@me.com>
2022-08-12 17:37:04 -0700

    Port GenericCallbacks to functional component

--- src/components/plugin-editor/GenericCallbacks.tsx
+++ src/components/plugin-editor/GenericCallbacks.tsx
@@ -1,5 +1,5 @@
 import { ICallback } from '@allejo/bzf-plugin-gen/dist';
-import React, { Component } from 'react';
+import React from 'react';
 
 import FormRepeater from '../common/FormRepeater';
 
@@ -7,40 +7,40 @@ interface Props {
   onChange: (callbacks: ICallback[]) => void;
 }
 
-export default class GenericCallbacks extends Component<Props> {
-  public _handleChange = (values: Record<string, string>[]): void => {
+const GenericCallbacks = ({ onChange }: Props) => {
+  const handleChange = (values: Record<string, string>[]) => {
     const callbacks = values.map<ICallback>((value: Record<string, string>) => ({
       name: value['callback'],
     }));
 
-    return this.props.onChange(callbacks);
+    onChange(callbacks);
   };
 
-  public _handleDisplayCallback = (value: Record<string, string>): JSX.Element => {
+  const handleDisplayCallback = (value: Record<string, string>): JSX.Element => {
     return <span>{value['callback']}</span>;
   };
 
-  public render(): React.ReactNode {
-    return (
-      <div>
-        <p>
-          Callbacks are used to allow plugins to communicate with each other. Transmitting and accepting data happens by
-          casting the <code>void*</code> data.
-        </p>
+  return (
+    <div>
+      <p>
+        Callbacks are used to allow plugins to communicate with each other. Transmitting and accepting data happens by
+        casting the <code>void*</code> data.
+      </p>
 
-        <FormRepeater onChange={this._handleChange} itemRendererCallback={this._handleDisplayCallback}>
-          <label htmlFor="callbacks">Add New Callback</label>
+      <FormRepeater onChange={handleChange} itemRendererCallback={handleDisplayCallback}>
+        <label htmlFor="callbacks">Add New Callback</label>
 
-          <input
-            type="text"
-            id="callbacks"
-            autoComplete="off"
-            className="form-control mt-1"
-            name="callback"
-            required={true}
-          />
-        </FormRepeater>
-      </div>
-    );
-  }
-}
+        <input
+          type="text"
+          id="callbacks"
+          autoComplete="off"
+          className="form-control mt-1"
+          name="callback"
+          required={true}
+        />
+      </FormRepeater>
+    </div>
+  );
+};
+
+export default GenericCallbacks;
