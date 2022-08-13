ba67d50ede6751097991699733c69a718ad53dac
Vladimir Jimenez <allejo@me.com>
2022-08-13 15:11:19 -0700

    Port BZDBSettings to functional component

--- src/components/plugin-editor/BZDBSettings.tsx
+++ src/components/plugin-editor/BZDBSettings.tsx
@@ -1,5 +1,5 @@
 import { BZDBType, IBZDBSetting, PluginBuilder } from '@allejo/bzf-plugin-gen/dist';
-import React, { Component, ReactNode } from 'react';
+import React from 'react';
 
 import FormRepeater from '../common/FormRepeater';
 
@@ -7,48 +7,48 @@ interface Props {
   onChange: (settings: IBZDBSetting[]) => void;
 }
 
-const BZDBTypes: { [key in BZDBType]: string } = {
+const BZDBTypes: Record<BZDBType, string> = {
   [BZDBType.Bool]: 'Boolean',
   [BZDBType.Double]: 'Double',
   [BZDBType.Int]: 'Integer',
   [BZDBType.String]: 'String',
 };
 
-export default class BZDBSettings extends Component<Props> {
-  private static castSettingType(type: BZDBType, value: string) {
-    switch (type) {
-      case BZDBType.Bool:
-        return value === 'true';
+function castSettingType(type: BZDBType, value: string) {
+  switch (type) {
+    case BZDBType.Bool:
+      return value === 'true';
 
-      case BZDBType.Double:
-        return +value;
+    case BZDBType.Double:
+      return +value;
 
-      case BZDBType.Int:
-        return Math.trunc(+value);
+    case BZDBType.Int:
+      return Math.trunc(+value);
 
-      case BZDBType.String:
-        return value;
-    }
+    case BZDBType.String:
+      return value;
   }
+}
 
-  public _handleChange = (values: Record<string, string>[]): void => {
-    const settings = values.map<IBZDBSetting>((value: Record<string, string>) => {
-      const type: BZDBType = +value['setting_type'];
-      const setting: IBZDBSetting = {
-        name: value['setting_name'],
-        type: type,
-        value: BZDBSettings.castSettingType(type, value['setting_value']),
-      };
+const BZDBSettings = ({ onChange }: Props) => {
+  const handleChange = (values: Record<string, string>[]): void => {
+    return onChange(
+      values.map<IBZDBSetting>((value: Record<string, string>) => {
+        const type: BZDBType = +value['setting_type'];
+        const setting: IBZDBSetting = {
+          name: value['setting_name'],
+          type: type,
+          value: castSettingType(type, value['setting_value']),
+        };
 
-      PluginBuilder.normalizeBZDBSetting(setting);
+        PluginBuilder.normalizeBZDBSetting(setting);
 
-      return setting;
-    });
-
-    return this.props.onChange(settings);
+        return setting;
+      }),
+    );
   };
 
-  public _handleDisplayCallback = (value: Record<string, string>): JSX.Element => {
+  const handleDisplayCallback = (value: Record<string, string>): JSX.Element => {
     const type: BZDBType = +value['setting_type'] as BZDBType;
 
     return (
@@ -58,50 +58,50 @@ export default class BZDBSettings extends Component<Props> {
     );
   };
 
-  public render(): ReactNode {
-    return (
-      <div>
-        <p>Plug-ins can register custom BZDB settings available to the server.</p>
+  return (
+    <div>
+      <p>Plug-ins can register custom BZDB settings available to the server.</p>
 
-        <FormRepeater onChange={this._handleChange} itemRendererCallback={this._handleDisplayCallback}>
-          <div className="row">
-            <div className="col">
-              <label htmlFor="setting_name">Setting Name</label>
-              <input
-                type="text"
-                id="setting_name"
-                autoComplete="off"
-                className="form-control mt-1"
-                name="setting_name"
-                required={true}
-              />
-            </div>
-
-            <div className="col">
-              <label htmlFor="setting_type">Type</label>
-              <select name="setting_type" id="setting_Type" className="form-control mt-1">
-                <option value="" disabled={true} />
-                <option value={BZDBType.Bool}>Boolean</option>
-                <option value={BZDBType.Double}>Double</option>
-                <option value={BZDBType.Int}>Integer</option>
-                <option value={BZDBType.String}>String</option>
-              </select>
-            </div>
-
-            <div className="col">
-              <label htmlFor="setting_value">Default Value</label>
-              <input
-                type="text"
-                id="setting_value"
-                autoComplete="off"
-                className="form-control mt-1"
-                name="setting_value"
-                required={true}
-              />
-            </div>
+      <FormRepeater onChange={handleChange} itemRendererCallback={handleDisplayCallback}>
+        <div className="row">
+          <div className="col">
+            <label htmlFor="setting_name">Setting Name</label>
+            <input
+              type="text"
+              id="setting_name"
+              autoComplete="off"
+              className="form-control mt-1"
+              name="setting_name"
+              required={true}
+            />
           </div>
-        </FormRepeater>
-      </div>
-    );
-  }
-}
+
+          <div className="col">
+            <label htmlFor="setting_type">Type</label>
+            <select name="setting_type" id="setting_Type" className="form-control mt-1">
+              <option value="" disabled={true} />
+              <option value={BZDBType.Bool}>Boolean</option>
+              <option value={BZDBType.Double}>Double</option>
+              <option value={BZDBType.Int}>Integer</option>
+              <option value={BZDBType.String}>String</option>
+            </select>
+          </div>
+
+          <div className="col">
+            <label htmlFor="setting_value">Default Value</label>
+            <input
+              type="text"
+              id="setting_value"
+              autoComplete="off"
+              className="form-control mt-1"
+              name="setting_value"
+              required={true}
+            />
+          </div>
+        </div>
+      </FormRepeater>
+    </div>
+  );
+};
+
+export default BZDBSettings;
