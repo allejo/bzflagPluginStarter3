f49cf32459385d2354a07cae376d633b3d8923d6
Vladimir Jimenez <allejo@me.com>
2022-08-13 15:44:35 -0700

    Port MapObjectEditor to functional component

--- src/components/plugin-editor/MapObjectEditor.tsx
+++ src/components/plugin-editor/MapObjectEditor.tsx
@@ -1,8 +1,7 @@
 import { IMapObject, MapArgumentType } from '@allejo/bzf-plugin-gen';
-import { IMapProperty } from '@allejo/bzf-plugin-gen/dist';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import update from 'immutability-helper';
-import React, { Component, ReactNode } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from 'reactstrap';
 
 import { uuidV4 } from '../../utilities/common';
@@ -12,147 +11,121 @@ interface Props {
   onChange: (objects: IMapObject[]) => void;
 }
 
-interface State {
-  mapObjects: Record<string, IMapObject>;
-}
+const MapObjectEditor = ({ onChange }: Props) => {
+  const [mapObjects, setMapObjects] = useState<Record<string, IMapObject>>({});
 
-export default class MapObjectEditor extends Component<Props, State> {
-  readonly state: State = {
-    mapObjects: {},
-  };
-
-  public _handleMapObjectChange = (data: IMapObject): void => {
-    this.setState(
-      update(this.state, {
-        mapObjects: {
-          [data.uuid]: {
-            $set: data,
-          },
+  const handleMapObjectChange = (data: IMapObject): void => {
+    setMapObjects(
+      update(mapObjects, {
+        [data.uuid]: {
+          $set: data,
         },
       }),
-      this.liftMapObjectInventory,
     );
   };
 
-  public _handleMapObjectDelete = (data: IMapObject): void => {
-    this.setState(
-      update(this.state, {
-        mapObjects: {
-          $unset: [data.uuid],
-        },
+  const handleMapObjectDelete = (data: IMapObject): void => {
+    setMapObjects(
+      update(mapObjects, {
+        $unset: [data.uuid],
       }),
-      this.liftMapObjectInventory,
     );
   };
 
-  public _handleNewMapObject = (): void => {
+  const handleNewMapObject = (): void => {
     const uuid = uuidV4();
 
-    this.setState(
-      update(this.state, {
-        mapObjects: {
-          [uuid]: {
-            $set: this.defaultObject(uuid),
+    setMapObjects(
+      update(mapObjects, {
+        [uuid]: {
+          $set: {
+            uuid: uuid,
+            name: 'object',
+            properties: [
+              {
+                uuid: uuidV4(),
+                name: 'position|pos',
+                readonly: true,
+                arguments: [
+                  {
+                    uuid: uuidV4(),
+                    name: 'x_pos',
+                    type: MapArgumentType.Float,
+                  },
+                  {
+                    uuid: uuidV4(),
+                    name: 'y_pos',
+                    type: MapArgumentType.Float,
+                  },
+                  {
+                    uuid: uuidV4(),
+                    name: 'z_pos',
+                    type: MapArgumentType.Float,
+                  },
+                ],
+              },
+              {
+                uuid: uuidV4(),
+                name: 'size',
+                readonly: true,
+                arguments: [
+                  {
+                    uuid: uuidV4(),
+                    name: 'x_size',
+                    type: MapArgumentType.Float,
+                  },
+                  {
+                    uuid: uuidV4(),
+                    name: 'y_size',
+                    type: MapArgumentType.Float,
+                  },
+                  {
+                    uuid: uuidV4(),
+                    name: 'z_size',
+                    type: MapArgumentType.Float,
+                  },
+                ],
+              },
+              {
+                uuid: uuidV4(),
+                name: 'rotation|rot',
+                readonly: true,
+                arguments: [
+                  {
+                    uuid: uuidV4(),
+                    name: 'rotation',
+                    type: MapArgumentType.Float,
+                  },
+                ],
+              },
+            ],
           },
         },
       }),
-      this.liftMapObjectInventory,
     );
   };
 
-  public render(): ReactNode {
-    const { mapObjects } = this.state;
+  useEffect(() => {
+    onChange(Object.values(mapObjects));
+  }, [mapObjects]);
 
-    return (
-      <div>
-        <p>
-          A plug-in can register custom map objects. These objects have no physical appearance and instead are zones
-          visible only to the server.
-        </p>
+  return (
+    <div>
+      <p>
+        A plug-in can register custom map objects. These objects have no physical appearance and instead are zones
+        visible only to the server.
+      </p>
 
-        {Object.entries(mapObjects).map((value: [string, IMapObject]) => (
-          <MapObject
-            key={value[0]}
-            value={value[1]}
-            onChange={this._handleMapObjectChange}
-            onDelete={this._handleMapObjectDelete}
-          />
-        ))}
+      {Object.entries(mapObjects).map((value: [string, IMapObject]) => (
+        <MapObject key={value[0]} value={value[1]} onChange={handleMapObjectChange} onDelete={handleMapObjectDelete} />
+      ))}
 
-        <Button color="primary" onClick={this._handleNewMapObject}>
-          <FontAwesomeIcon icon="plus" className="mr-2" />
-          Add Map Object
-        </Button>
-      </div>
-    );
-  }
+      <Button color="primary" onClick={handleNewMapObject}>
+        <FontAwesomeIcon icon="plus" className="mr-2" />
+        Add Map Object
+      </Button>
+    </div>
+  );
+};
 
-  private liftMapObjectInventory = (): void => {
-    this.props.onChange(Object.values(this.state.mapObjects));
-  };
-
-  private defaultObject = (uuid: string): IMapObject => ({
-    uuid: uuid,
-    name: 'object',
-    properties: this.defaultProperties(),
-  });
-
-  private defaultProperties = (): IMapProperty[] => [
-    {
-      uuid: uuidV4(),
-      name: 'position|pos',
-      readonly: true,
-      arguments: [
-        {
-          uuid: uuidV4(),
-          name: 'x_pos',
-          type: MapArgumentType.Float,
-        },
-        {
-          uuid: uuidV4(),
-          name: 'y_pos',
-          type: MapArgumentType.Float,
-        },
-        {
-          uuid: uuidV4(),
-          name: 'z_pos',
-          type: MapArgumentType.Float,
-        },
-      ],
-    },
-    {
-      uuid: uuidV4(),
-      name: 'size',
-      readonly: true,
-      arguments: [
-        {
-          uuid: uuidV4(),
-          name: 'x_size',
-          type: MapArgumentType.Float,
-        },
-        {
-          uuid: uuidV4(),
-          name: 'y_size',
-          type: MapArgumentType.Float,
-        },
-        {
-          uuid: uuidV4(),
-          name: 'z_size',
-          type: MapArgumentType.Float,
-        },
-      ],
-    },
-    {
-      uuid: uuidV4(),
-      name: 'rotation|rot',
-      readonly: true,
-      arguments: [
-        {
-          uuid: uuidV4(),
-          name: 'rotation',
-          type: MapArgumentType.Float,
-        },
-      ],
-    },
-  ];
-}
+export default MapObjectEditor;
