c90305362cd7e3b8ed3ade757383ec38f0530483
Vladimir Jimenez <allejo@me.com>
2022-08-13 23:12:40 -0700

    Update to React 18 and a lot of other dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,3 @@
-import 'typeface-bitter';
-import 'typeface-raleway';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import './index.scss';
-
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faAngleDown,
@@ -14,15 +8,22 @@ import {
   faTimesCircle,
   faTrashAlt,
 } from '@fortawesome/free-solid-svg-icons';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import 'typeface-bitter';
+import 'typeface-raleway';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import './index.scss';
+
 library.add(faAngleDown, faAngleRight, faClipboard, faDownload, faPlus, faTimesCircle, faTrashAlt);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
