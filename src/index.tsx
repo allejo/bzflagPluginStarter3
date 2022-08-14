678b63df9ab7d156c1122ddfa587b3f5e5fa28fa
Vladimir Jimenez <allejo@me.com>
2022-08-14 00:45:00 -0700

    Fix exceptions because of immer's auto-freezing

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { setAutoFreeze } from 'immer';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'typeface-bitter';
@@ -9,6 +10,12 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.scss';
+
+// We're using immer to manipulate objects that are stored in state. However,
+// not all objects are and immer's automatic freezing functionality can lead to
+// problems for those that are not. So let's disable it.
+//   https://immerjs.github.io/immer/freezing/
+setAutoFreeze(false);
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
